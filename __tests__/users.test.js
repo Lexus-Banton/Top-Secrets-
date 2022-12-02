a0d2d5b80083ac80bb3fa90792e7a6c80314d7ca
Lex <106777728+Lexus-Banton@users.noreply.github.com>
2022-12-02 14:46:03 -0800

    adds sign in using jwts ands cookies

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
 
 describe('user routes', () => {
   beforeEach(() => {
@@ -17,6 +18,22 @@ describe('user routes', () => {
       id: expect.any(String),
       email: 'test@example.com',
     });
+  });
+
+  it('POST /api/v1/users/sessions should log in an existing user', async () => {
+    // use the User Service to create a new user
+    const mockUser = {
+      email: 'test@example.com',
+      password: '123456',
+    };
+    await UserService.create(mockUser);
+    // log that user in
+    const resp = await request(app)
+      .post('/api/v1/users/sessions')
+      .send(mockUser);
+    // confirm that I get a 200
+    expect(resp.status).toBe(200);
+    expect(resp.body).toEqual({ message: 'Signed in successfully!' });
   });
 
   afterAll(() => {
